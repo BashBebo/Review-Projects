d69a7e2bf5e93d2bab1931c5c33e1a6ba6da7750
Bashir <bashir6412@gmail.com>
2026-04-04 20:55:35 +0400

    JustRevieiwngByMakingNewCode

---
+++ index.js
@@ -0,0 +1,20 @@
+let InputTask = document.getElementById("type_task");
+let AddTask = document.getElementById("add_button");
+let ValidationText = document.getElementById("validation_text");
+let TaskList = document.getElementById("task_list");
+let OrderList = document.getElementById("odl");
+
+AddTask.addEventListener("click", function () {
+    let InputValue = InputTask.value
+    if (InputValue === "") {
+        ValidationText.textContent = "Please enter a task name.";
+        
+    } else {
+      InputTask.value = "";
+      ValidationText.textContent = "";
+      let TaskItem = document.createElement("p");
+      TaskItem.textContent = InputValue;
+      OrderList.appendChild(TaskItem);
+      console.log(TaskItem);
+    }
+})
